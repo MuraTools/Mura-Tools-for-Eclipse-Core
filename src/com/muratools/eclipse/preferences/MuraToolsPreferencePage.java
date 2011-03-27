a5645e0456cafa33fedaf53493ae16558fc76689
Steve Good <steve@stevegood.org>
2011-03-27 10:49:24 -0500

    Cleaned up code and added license text.

--- src/com/muratools/eclipse/preferences/MuraToolsPreferencePage.java
+++ src/com/muratools/eclipse/preferences/MuraToolsPreferencePage.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2011 Steve Good [steve@stevegood.org]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.muratools.eclipse.preferences;
 
 import org.eclipse.jface.preference.*;
@@ -48,8 +64,7 @@ public class MuraToolsPreferencePage
 				PreferenceConstants.P_CHOICE,
 			"An example of a multiple-choice preference",
 			1,
-			new String[][] { { "&Choice 1", "choice1" }, {
-				"C&hoice 2", "choice2" }
+			new String[][] { { "&Choice 1", "choice1" }, { "C&hoice 2", "choice2" }
 		}, getFieldEditorParent()));
 		addField(
 			new StringFieldEditor(PreferenceConstants.P_STRING, "A &text preference:", getFieldEditorParent()));
--- src/com/muratools/eclipse/preferences/PreferenceInitializer.java
+++ src/com/muratools/eclipse/preferences/PreferenceInitializer.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2011 Steve Good [steve@stevegood.org]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.muratools.eclipse.preferences;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
@@ -19,8 +35,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
 		store.setDefault(PreferenceConstants.P_BOOLEAN, true);
 		store.setDefault(PreferenceConstants.P_CHOICE, "choice2");
-		store.setDefault(PreferenceConstants.P_STRING,
-				"Default value");
+		store.setDefault(PreferenceConstants.P_STRING, "Default value");
 	}
 
 }
